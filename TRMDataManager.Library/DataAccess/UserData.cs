446def4ae56fe1805326a65abd57f7a48b97355e
Florentina Ionescu <ionescoflorin@gmail.com>
2023-01-11 02:10:57 +0200

    Getting User Data part 1 - debugged database connection

---
+++ TRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManager.Library.Internal.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            //anonymous object with no named type
+            var p = new { Id = Id };
+
+            //dynamic works only within the same assembly
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "TRMData");
+
+            return output;
+        }
+    }
+}
---
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDataManager.Library.Internal.DataAccess
+{
+    internal class SqlDataAccess
+    {
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        // T, U = generics
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                // makes connection to db, it does a query, it stays this is the type of model
+                // that we want each row to be, pass stored procedure name and parameters which is type U,
+                // full connection string name, says this is a stored procedure and it is going to return
+                // a set of rows
+                List<T> rows = connection.Query<T>(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+               connection.Execute(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
