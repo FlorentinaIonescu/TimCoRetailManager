393d4f2b395e9a226a931e44ac6894bb01bc4295
Florentina Ionescu <ionescoflorin@gmail.com>
2023-01-13 03:47:06 +0200

    Early Refactoring in C#. Add FK relationships on tables, update EFDatabase, replace container reference with IoC, simplify tax calculation, add additional item info

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -13,18 +13,16 @@ namespace TRMDesktopUI.ViewModels
     {
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
 
         // construction injection
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, SimpleContainer container)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
             _events = events;
             _salesVM = salesVM;
-            _container = container;
 
             _events.Subscribe(this);
 
-            ActivateItemAsync(_container.GetInstance<LoginViewModel>());
+            ActivateItemAsync(IoC.Get<LoginViewModel>());
 
         }
 
