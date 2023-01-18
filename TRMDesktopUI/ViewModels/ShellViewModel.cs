0e35547b56c806553c3fea150280433c766abfe8
Florentina Ionescu <ionescoflorin@gmail.com>
2023-01-18 04:20:30 +0200

    Displaying Users and Groups/Roles. To do: internal server error fix

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -55,6 +55,11 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        public void UserManagement()
+        {
+            ActivateItemAsync(IoC.Get<UserDisplayViewModel>());
+        }
+
         public void LogOut()
         {
             _user.ResetUserModel();
